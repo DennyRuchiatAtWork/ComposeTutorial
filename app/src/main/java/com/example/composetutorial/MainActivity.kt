d573befa28ce32ca68b4be3784e4136dfff81e3c
denny_imperialhealthtech <denny@Imperialhealthtech.com>
2024-05-20 10:34:53 +0700

    Add: Image element

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -12,18 +16,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 //            Text("Hello World!")
-            MessageCard("Android")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact Profile Picture"
+        )
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview
 @Composable
-fun PreviewMessage() {
-    MessageCard(name = "Android Preview")
+fun PreviewMessageCard() {
+    MessageCard(Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!"))
 }
